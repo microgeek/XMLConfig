4a1d5677c562d4e716b66ed8474dca015f5fdde7
Micro Geek <microgeek@Micros-Mac-mini.local>
2013-05-21 09:24:35 -0400

    Changed tab style

--- src/me/microgeek/xmlconfig/XMLConfigLib.java
+++ src/me/microgeek/xmlconfig/XMLConfigLib.java
@@ -4,24 +4,23 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class XMLConfigLib extends JavaPlugin {
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.bukkit.plugin.java.JavaPlugin#onDisable()
-	 */
-	@Override
-	public void onDisable() {
+  /*
+   * (non-Javadoc)
+   * @see org.bukkit.plugin.java.JavaPlugin#onDisable()
+   */
+  @Override
+  public void onDisable() {
 
-	}
+  }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
-	 */
-	@Override
-	public void onEnable() {
-		/**
-		 * for files in plugins/plugin that end in .xml
-		 *   XMLConfig.loadNewConfig
-		 */
-	}
+  /*
+   * (non-Javadoc)
+   * @see org.bukkit.plugin.java.JavaPlugin#onEnable()
+   */
+  @Override
+  public void onEnable() {
+    /**
+     * for files in plugins/plugin that end in .xml XMLConfig.loadNewConfig
+     */
+  }
 }
